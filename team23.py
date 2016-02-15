940dc688e9176a95f40b022e5754b51baf9e624d
rehassachdeva <aquannie@gmail.com>
2016-02-15 19:16:38 +0530

    Added function to return valid cells

--- team23.py
+++ team23.py
@@ -14,8 +14,35 @@ class Player23:
 
         def blocks_allowed(old_move, temp_block):
             pass
+        
         def cells_allowed(temp_board, blocks_allowed):
-            pass
+            
+            cells = []
+
+            for block in blocks_allowed:
+                
+                start_row = (block / 3) * 3
+                start_col = ((block) % 3) * 3
+
+                for i in range(start_row, start_row + 3):
+                    for j in range(start_col, start_col + 3):
+                        if temp_board[i][j] == '-':
+                            cells.append((i,j))
+
+            if not cells:
+
+                for i in range(9):
+
+                    start_row = (i / 3) * 3
+                    start_col = ((i) % 3) * 3
+                    
+                    for j in range(start_row, start_row + 3):
+                        for k in range(start_col, start_col + 3):
+                            if temp_board[j][k] == '-':
+                                cells.append((j,k))
+
+            return cells
+
 
 
 
